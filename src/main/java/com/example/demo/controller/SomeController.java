0119f41a241345b1aca02dd309b6d9e2f6eaca91
abdullah <Abdullahamzeh@hotmail.com>
2022-08-04 00:14:26 -0500

    initial

---
+++ src/main/java/com/example/demo/controller/SomeController.java
@@ -0,0 +1,58 @@
+package com.example.demo.controller;
+
+import com.example.demo.entity.Customer;
+import com.example.demo.repository.CustomerRepository;
+import org.keycloak.adapters.springsecurity.token.KeycloakAuthenticationToken;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
+import java.security.Principal;
+
+@RestController
+public class SomeController {
+    @Autowired
+    private CustomerRepository repo;
+
+    @GetMapping(path = "/")
+    public String index() {
+        return "external";
+    }
+
+    @GetMapping("/logout")
+    public String logout(HttpServletRequest request) throws Exception {
+        request.logout();
+        return "redirect:/";
+    }
+
+    @GetMapping(path = "/customers")
+    public Iterable<Customer> customers() {
+        addCustomers();
+        Iterable<Customer> customers = repo.findAll();
+        return customers;
+    }
+
+    public void addCustomers() {
+
+        Customer customer1 = new Customer();
+        customer1.setAddress("1111 foo blvd");
+        customer1.setName("Foo Industries");
+        customer1.setServiceRendered("Important services");
+        repo.save(customer1);
+
+        Customer customer2 = new Customer();
+        customer2.setAddress("2222 bar street");
+        customer2.setName("Bar LLP");
+        customer2.setServiceRendered("Important services");
+        repo.save(customer2);
+
+        Customer customer3 = new Customer();
+        customer3.setAddress("33 main street");
+        customer3.setName("Big LLC");
+        customer3.setServiceRendered("Important services");
+        repo.save(customer3);
+    }
+}
---
+++ src/main/java/com/example/demo/controller/SomeOtherController.java
@@ -0,0 +1,46 @@
+package com.example.demo.controller;
+
+import com.example.demo.entity.UserInfo;
+import org.keycloak.KeycloakPrincipal;
+import org.keycloak.KeycloakSecurityContext;
+import org.keycloak.adapters.springsecurity.token.KeycloakAuthenticationToken;
+import org.keycloak.representations.IDToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.security.Principal;
+import java.util.Map;
+
+@RestController
+public class SomeOtherController {
+    @GetMapping(path = "/users")
+    public UserInfo getUserInfo() {
+
+        KeycloakAuthenticationToken authentication = (KeycloakAuthenticationToken) SecurityContextHolder.getContext()
+                .getAuthentication();
+
+        final Principal principal = (Principal) authentication.getPrincipal();
+
+        String dob = "";
+        String userIdByToken = "";
+        String userIdByMapper = "";
+
+        if (principal instanceof KeycloakPrincipal) {
+            KeycloakPrincipal<KeycloakSecurityContext> kPrincipal = (KeycloakPrincipal<KeycloakSecurityContext>) principal;
+            var token = kPrincipal.getKeycloakSecurityContext().getToken();
+
+            userIdByToken = token.getSubject();
+            userIdByMapper = token.getOtherClaims().get("sid").toString();
+
+            Map<String, Object> customClaims = token.getOtherClaims();
+
+            if (customClaims.containsKey("DOB")) {
+                dob = String.valueOf(customClaims.get("DOB"));
+            }
+        }
+
+        return new UserInfo(principal.getName(), userIdByToken, userIdByMapper, dob);
+    }
+}
