ca530c87c518a3124a9fc33ea35d50a638253eb4
yousefsalem <yousef_salem97@yahoo.com>
2022-08-05 19:10:23 -0500

    add swagger

--- Backend/src/main/java/com/example/demo/controller/ProfileController.java
+++ Backend/src/main/java/com/example/demo/controller/ProfileController.java
@@ -2,6 +2,7 @@ package com.example.demo.controller;
 
 import com.example.demo.model.ProfileModel;
 import com.example.demo.service.interfaces.ProfileService;
+import io.swagger.annotations.Api;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
