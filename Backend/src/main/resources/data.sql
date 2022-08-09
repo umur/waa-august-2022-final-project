3f2e97f077e73d614f09df9862f33cc809e8a20a
AbdullahAlatiyat <abdallahamzeh@hotmail.com>
2022-08-08 23:32:07 -0500

    fix

--- Backend/src/main/resources/data.sql
+++ Backend/src/main/resources/data.sql
@@ -8,16 +8,16 @@ insert into department(id,name) values(2, 'Business');
 
 insert into department(id,name) values(3, 'Science');
 
-insert into profile(id, first_name, is_deleted, last_name, profile_type, profilekclock_id)
-values (1, 'yousef', false, 'salem', 1, '8a774910-8cdd-4e83-8ace-207b64bfcd38');
-
-insert into student(id, gpa, department_id, profile_id, studentkclock_id)
-values (1, 3.0, 1, 1, '11');
-
-insert into job_advertisement(id, benefits, city, company_name, description, state)
-values ( 1, 'benefits', 'city', 'company', 'description', 'state');
-insert into job_advertisement(id, benefits, city, company_name, description, state)
-values ( 2, 'benefits2', 'city2', 'company2', 'description2', 'state2');
-
+-- insert into profile(id, first_name, is_deleted, last_name, profile_type, profilekclock_id)
+-- values (1, 'yousef', false, 'salem', 1, '8a774910-8cdd-4e83-8ace-207b64bfcd38');
+--
+-- insert into student(id, gpa, department_id, profile_id, studentkclock_id)
+-- values (1, 3.0, 1, 1, '11');
+--
+-- insert into job_advertisement(id, benefits, city, company_name, description, state)
+-- values ( 1, 'benefits', 'city', 'company', 'description', 'state');
+-- insert into job_advertisement(id, benefits, city, company_name, description, state)
+-- values ( 2, 'benefits2', 'city2', 'company2', 'description2', 'state2');
+--
 
 
